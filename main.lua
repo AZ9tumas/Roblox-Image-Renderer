af1183ea8389668bcc5b1a5190090ebacd694604
AZ9tumas <77965895+AZ9tumas@users.noreply.github.com>
2023-04-07 14:13:59 +0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,41 +7,20 @@ local RayCastModule = require(script.Parent.ModuleScript)
 local start = Vector3.new(50, 0, 50)
 local endPosition = Vector3.new(-50, 0, -50)
 
-local incrementx = 0.5
-local incrementz = 0.5
+local incrementx = 0.125
+local incrementz = 0.125
 
 -- This function sends the color information and some other data to the NodeJs server
-function SendInformation(Path, DataToSend)
+function SendInformation(Path : string, DataToSend : {})
 	local Encoded = Http:JSONEncode(DataToSend)
 	local Response = Http:PostAsync(Link..Path, Encoded, Enum.HttpContentType.ApplicationJson)
 
 	return Response
 end
 
-function get_table_size(table)
-	-- Calculates the size of a table (array of arrays) in bytes.
-	local total_size = 0
-
-	-- Calculate the size of the outer table
-	total_size = total_size + #table * 44 -- Assuming each element takes 12 bytes
-
-	for i = 1, #table do
-		local row = table[i]
-		-- Calculate the size of each inner table
-		total_size = total_size + #row * 8  -- Assuming each element takes 8 bytes
-
-		for j = 1, #row do
-			local item = row[j]
-			-- Calculate the size of each item in the inner table
-			if type(item) == "number" then
-				total_size = total_size + 8  -- For numbers, assuming 8 bytes
-			elseif type(item) == "string" then
-				total_size = total_size + #item  -- For strings, add the length in bytes
-			end
-		end
-	end
-
-	return total_size
+function get_size(table : {[any]:Color3})
+	-- Calculates the size of a table in bytes.
+	return #table * 44
 end
 
 local Data = {}
@@ -55,8 +34,18 @@ for x = start.X, endPosition.X, (endPosition.X < start.X and -incrementx or incr
 		table.insert(RowColorData, ColorDetails)
 	end
 	table.insert(Data, RowColorData)
-	TotalSize += get_table_size(RowColorData)
+	TotalSize += get_size(RowColorData)
+	
+	if (TotalSize / 1024) > 1000 then
+		print("Splitting")
+		SendInformation("/",  {["ColorInformation"] = Data, ["Render"] = false})
+		Data = {}
+		TotalSize = 0
+	end
 end
 
-SendInformation("/", {["ColorInformation"] = Data})
+print("Total Size: ", TotalSize / 1024)
+if Data ~= {} then
+	SendInformation("/", {["ColorInformation"] = Data, ["Render"] = true})
+end
 print("Data Obtained:", Data)
